3b50c08bf6f51beba0b241a8ab36ec20bc176402
hafid-firman-febrian <hafidfirmanfebrian.dev@gmail.com>
2026-06-19 23:06:14 +0700

    refactor(salary_period): standardize import paths for salary_period_model and introduce SalaryPeriodService for improved salary period management

--- lib/features/salary_period/data/repositories/salary_period_repository.dart
+++ lib/features/salary_period/data/repositories/salary_period_repository.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pixel_pocket/core/error/failure.dart';
 import 'package:pixel_pocket/features/salary_period/data/datasources/salary_period_remote_data_source.dart';
-import 'package:pixel_pocket/features/salary_period/domain/model/salary_period_model.dart';
+import 'package:pixel_pocket/features/salary_period/domain/models/salary_period_model.dart';
 
 class SalaryPeriodRepository {
   SalaryPeriodRepository(this._remote);
